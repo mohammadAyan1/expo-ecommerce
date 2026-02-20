8a00a878ea066628aa7be91652150a8c323f81fe
mohammadAyan1 <mohammadayan2210@gmail.com>
2026-02-20 06:23:20 +0530

    auth-setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,21 @@ import express from "express";
 import path from "path"
 import { ENV } from "./config/env.js";
 import { connectionDB } from "./config/db.js";
-
+import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
+import { functions, inngest } from "./config/innegest.js";
 
 const app = express()
 
 const __dirname = path.resolve()
-
+app.use(express.json())
 app.use(clerkMiddleware())
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "server is running" })
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }))
 
 if (ENV.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../admin/dist")))
